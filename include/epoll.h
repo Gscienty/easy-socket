260a7c325ec69f68722f24d64bf1290a2a461356
Gscienty <gaoxiaochuan@hotmail.com>
2018-04-15 23:56:59 +0800

    try add epoll

---
+++ include/epoll.h
@@ -0,0 +1,14 @@
+#ifndef _EYS_EPOLL_
+#define _EYS_EPOLL_
+
+#include "define.h"
+#include "base_fd.h"
+#include <sys/epoll.h>
+#include <memory>
+
+namespace eys {
+    class epoll {
+    };
+}
+
+#endif
