2d5aa63582b10ac710e2d8d56674e9812a80917f
Gscienty <gaoxiaochuan@hotmail.com>
2018-05-02 16:13:54 +0800

    change buffer type to single byte type pointer

--- include/littleendian_serializer.h
+++ include/littleendian_serializer.h
@@ -1,25 +1,23 @@
-#ifndef _EYS_BIG_ENDIAN_SERIALIZER_
-#define _EYS_BIG_ENDIAN_SERIALIZER_
+#ifndef _EYS_LITTLE_ENDIAN_SERIALIZER_
+#define _EYS_LITTLE_ENDIAN_SERIALIZER_
 
 #include <stddef.h>
-#include <string>
 #include <utility>
 
 namespace eys {
     template <typename BufferType, typename ElementType>
-    static void __inl_bigendian_serialize(BufferType v, ElementType e) {
+    static void __inl_littleendian_serialize(BufferType v, ElementType e) {
         for (size_t i = 0; i < sizeof(ElementType); i++) {
-            v[i]  = reinterpret_cast<BufferType>(&e)[sizeof(ElementType) - 1 - i];
+            v[i] = reinterpret_cast<BufferType>(&e)[i];
         }
     }
 
     template <typename BufferType, typename ElementType>
-    static ElementType  __inl_bigendian_deserialize(
+    static void __inl_littleendian_deserialize(
             const BufferType v, const size_t len, size_t &seek) {
         ElementType result;
         for (size_t i = 0; i < sizeof(ElementType); i++) {
-            reinterpret_cast<BufferType>(&result)[sizeof(ElementType) - 1 - i] = 
-                v[seek];
+            reinterpret_cast<BufferType>(&result)[i] = v[seek];
             seek++;
             if (seek >= len) {
                 return result;
@@ -29,22 +27,18 @@ namespace eys {
     }
 
     template <typename SingleByteType, typename ElementType>
-    struct bigendian_serializer {
+    struct littleendian_serializer {
         static std::pair<SingleByteType *, size_t> serialize(ElementType e) {
             size_t size = sizeof(ElementType);
             SingleByteType *buffer = new SingleByteType[size];
-
-            __inl_bigendian_serialize(buffer, e);
-
+            __inl_littleendian_serialize(buffer, e);
             return std::pair<SingleByteType *, size_t>(buffer, size);
         }
-
         static ElementType deserialize(
                 const SingleByteType *buffer, size_t len, size_t &seek) {
-            return __inl_bigendian_deserialize<SingleByteType *, ElementType>(buffer, len, seek);
+            return __inl_littleendian_deserialize<SingleByteType *, ElementType>(buffer, len, seek);
         }
     };
 }
 
 #endif
-
